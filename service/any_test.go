131b91888a4220448a57b8c737c543a2210cd68e
Komei1009 <1994komei1009@gmail.com>
2018-11-29 16:42:46 +0900

    .

--- service/any_test.go
+++ service/any_test.go
@@ -3,6 +3,5 @@ package service
 import "testing"
 
 func TestHit(t *testing.T) {
-
-	Hit.send("https://img-mdpr.freetls.fastly.net/article/TIwx/nm/TIwxtNFEo2C0G5LFu0VSLLOGUpc1h_PNUde6zZmxoT4.jpg?auto=webp")
+	Hit.Set("programmer", true)
 }
