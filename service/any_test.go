f5eba9a564dedc58445a5e90f1942e8106b25391
makki0205 <llxo2_5oxll@icloud.com>
2018-11-29 17:51:29 +0900

    .

--- service/any_test.go
+++ service/any_test.go
@@ -3,6 +3,6 @@ package service
 import "testing"
 
 func TestHit(t *testing.T) {
-
-	Hit.send("https://img-mdpr.freetls.fastly.net/article/TIwx/nm/TIwxtNFEo2C0G5LFu0VSLLOGUpc1h_PNUde6zZmxoT4.jpg?auto=webp")
+	Led.SetAll(200, 200, 200)
+	//Hit.send("https://img-mdpr.freetls.fastly.net/article/TIwx/nm/TIwxtNFEo2C0G5LFu0VSLLOGUpc1h_PNUde6zZmxoT4.jpg?auto=webp")
 }
--- service/building.go
+++ service/building.go
@@ -1,1 +1,34 @@
 package service
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/makki0205/log"
+
+	"github.com/makki0205/config"
+)
+
+var Led ledService
+
+type ledService struct {
+	url string
+}
+
+func init() {
+	res := ledService{}
+	res.url = config.Env("led_url")
+}
+
+func (l *ledService) SetAll(r, g, b uint8) {
+	req, _ := json.Marshal(struct {
+		R uint8 `json:"r"`
+		G uint8 `json:"g"`
+		B uint8 `json:"b"`
+	}{r, g, b})
+	_, err := http.Post(l.url+"/", "application/json", bytes.NewReader(req))
+	if err != nil {
+		log.Err(err)
+	}
+}
